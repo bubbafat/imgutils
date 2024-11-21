89693259961e54f7486444f9e980d60139e65a95
Robert Horvick <robert.horvick@gmail.com>
2024-11-21 15:22:37 -0500

    Python 3

--- procpath.sh
+++ procpath.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-for file in $1/*.jpg; python frame.py -input $file -output $file:h/framed/$file:t -caption "{Make} {Model} {LensModel}" -caption "{FocalLength}mm 1/{ExposureTime}, f/{FNumber}, ISO: {ISOSpeedRatings}" -font Helvetica -method 8% 
+for file in $1/*.jpg; python3 frame.py -input $file -output $file:h/framed/$file:t -caption "{Make} {Model} {LensModel}" -caption "{FocalLength}mm 1/{ExposureTime}, f/{FNumber}, ISO: {ISOSpeedRatings}" -font Helvetica -long 2048 -method 8% 
 
